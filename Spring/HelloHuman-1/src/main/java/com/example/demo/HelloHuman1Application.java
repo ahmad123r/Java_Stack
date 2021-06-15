f30499b3d945cba4f7c30853e1db4d40db97a7a8
ahmad123r <ahmadzero___@hotmail.com>
2021-06-15 15:24:13 +0300

    Jeva

---
+++ Spring/HelloHuman-1/src/main/java/com/example/demo/HelloHuman1Application.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloHuman1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloHuman1Application.class, args);
+	}
+	@RequestMapping("/")
+    public String index(@RequestParam(value="q", required=false) String searchQuery) {
+		if(searchQuery != null)
+			return "Hello " + searchQuery+"!";
+		else {
+			
+		searchQuery="Human";
+		return "Hello " + searchQuery+"!";
+		}
+    }
+}
